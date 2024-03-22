04462fe75358e5f9820b8b68c56b2e9b31d63b60
burioden <hashizoe.y@gmail.com>
2024-03-23 06:15:00 +0900

    vercellエラー対応

--- rps-app/src/pages/settings.js
+++ rps-app/src/pages/settings.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
 import Link from "next/link";
+import Head from "next/head";
 import { SettingsContent } from "../../compornents/settingsContent";
 
 export default function Home() {
